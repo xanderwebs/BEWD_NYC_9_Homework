db9ba836f3c7dffac723dd29986a8654f9c45644
NINA SAPORTANINA SAPORTAgit config --global user.name NINA <NinaSaporta@gmail.com>
2014-03-30 12:18:38 -0400

    Homework complete except for storing total_game variable.  Commented out lines that I thought would work but are not running properly

--- Nina_Saporta_HW_01.rb
+++ Nina_Saporta_HW_01.rb
@@ -1,3 +1,5 @@
+$total_games= 0
+
 def welcome_message
   puts "Welcome to the secret number guessing game!!  This game was created by aspiring developer Nina Saporta!"
 end
@@ -14,30 +16,32 @@ def secret_number
 	3
 end
 
-def ask_user_guess
-	total_games = 3
+#This method asks the user to enter a number then uses conditionals to determine if it's the correct number or not.  
+#I can't get the variable total_games to start at 0 then to store the updated value each time the function is run.  Commented out lines are what I had programmed, but was not working. 
+
+def ask_user_guess ()
 	puts "Please guess a nunmber between 1 and 10!"
 	user_guess = gets
 	user_guess = user_guess.chomp.to_i
-	if user_guess ==3
+	if user_guess == 3
 	puts "That's correct! Congratulations!!! Game over!"
 	elsif
 	user_guess > 3
-	total_games = total_games -1
-	puts "Too high! Guess again You have #{total_games} left"
-		if total_games > 0
+	#total_games = total_games + 1
+		#if total_games  <= 3
+		puts "Too high! Guess again! You have #{} games left"
 		ask_user_guess
-		else
-		puts "Sorry, no more guesses.  Game over!"
-		end
+		#else
+	#	puts "Sorry, no more guesses.  The correct answer was 3. Game over!"
+	#	end
 	else
-	total_games = total_games -1
-	puts "Too low! Guess again You have #{total_games} left"
-		if total_games > 0
+	#total_games = total_games + 1
+	#	if total_games <= 3
+		puts "Too low! Guess again You have #{} games left"
 		ask_user_guess
-		else
-		puts "Sorry, no more guesses.  Game over!"
-		end
+	#	else
+	#	puts "Sorry, no more guesses.  The correct answer was 3.  Game over!"
+	#	end
 	end
 end
 
@@ -52,3 +56,4 @@ puts "Hello " + playername + "get ready to play!"
 puts game_rules
 
 ask_user_guess 
+
