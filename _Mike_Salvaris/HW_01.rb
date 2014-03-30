dd48427b249c718ba4b8fd02b9b94a27b12cad7a
Mike Salvaris <mike_salv1@hotmail.com>
2014-03-30 16:39:43 -0400

    Uploading homework

--- _Mike_Salvaris/HW_01.rb
+++ _Mike_Salvaris/HW_01.rb
@@ -36,3 +36,36 @@
 #
 ###############################################################################
 
+# Welcomes users and gets their name and first guess
+puts "Welcome to the Secert Number game created by Mike Salvaris"
+puts "What is your name?"
+first_name = gets.chomp
+first_name.capitalize!
+puts "Welcome #{first_name}"
+puts "Here are the rules: You have 3 tries to guess a number between 1 and 10"
+puts "What's your first guess?"
+guess = gets.chomp
+guess = guess.to_i
+# Establishes secret number and guess count 
+secret_number = rand(1..10)
+guess_count = 3
+# Loop that gives feedback to users if they are too low or too high, and adjusts the count of guesses accordingly.  If the guess count is 0 then game is ended
+while guess != secret_number do
+  if guess < secret_number
+	  puts "Nope! Higher"
+	  guess_count = guess_count -1
+  else guess > secret_number
+	  puts "Nope! Lower"
+	  guess_count = guess_count -1
+  end
+  abort("Sorry, you lost! Game over.  The secret number was #{secret_number}") unless guess_count > 0
+  puts "You only have #{guess_count} guesses left!"
+  puts "What's your next guess?"
+  guess = gets.chomp
+  guess = guess.to_i
+end
+# Executes if successful guess
+if guess == secret_number
+  puts "Congratulations you've won!"
+end
+
