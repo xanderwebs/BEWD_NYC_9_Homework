69031b4c3ed7559170f20bcdd92d2e41ea0fc75c
Max Dana <max@dissentertainment.com>
2014-04-01 17:43:36 -0400

    Created Secret Number game

--- _Max_Dana/HW_01.rb
+++ _Max_Dana/HW_01.rb
@@ -36,3 +36,57 @@
 #
 ###############################################################################
 
+# Welcome Player
+def welcome_player
+  puts "Welcome to Secret Number, a fake game by Max Dana."
+  print "Please enter your name to play: "
+  name = gets.chomp
+  puts "Welcome, #{name}.\nThe rules of Secret Number are very simple.\nI think of a secret number, you try to guess it.\nYou have three guesses."
+  return name
+end
+
+
+def play_game
+  # Initialize guess count and secret number
+  player_name = welcome_player
+  guess_count = 3
+  number = rand(1..10)
+
+  # Initialize player messages, guess count and secret number
+  msg_start = "I'm thinking of a number between 1 and 10.\nWhat is your first guess, #{player_name}?"
+  msg_high = "That's a bit too high."
+  msg_low = "That's a bit too low."
+  msg_win = "OMG, you totally read my mind. Congratulations, #{player_name}! :-)"
+  msg_lose = "LOL, you're not very good at this. Try again some other time, #{player_name}."
+
+  # Get guess from player and check to see if it matches number
+  while guess_count > 0 do
+  
+    # Display appropriate message for guess_count
+    if guess_count == 3
+  	  puts "#{msg_start}"
+    else
+      puts "You have #{guess_count} guesses left. What is your next guess?"
+    end
+  
+    # Get guess from player and decrease guess_count
+    guess = gets.chomp.to_i
+    guess_count -= 1
+    
+    # Check to see if guess is a winner  
+    if guess == number
+  	  puts "#{msg_win}"
+  	  guess_count = 0 # not sure how to get out of this loop so setting guess_count to 0
+    elsif guess > number && guess_count != 0
+      puts "#{msg_high}"
+    elsif guess < number && guess_count != 0
+      puts "#{msg_low}"
+    else
+  	  puts "#{msg_lose}"
+    end
+  end
+end
+
+
+play_game
+
