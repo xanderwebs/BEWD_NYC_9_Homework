cf5127ae06802b9fee5a3fe5b774a24e89d5e80f
james.shook@gmail.com <james.shook@gmail.com>
2014-04-03 17:07:49 -0400

    updated secret.rb

--- _Jim_Shook/secret.rb
+++ _Jim_Shook/secret.rb
@@ -3,7 +3,7 @@ puts "Welcome to the Secret Game!"
 puts "Please tell me your name"
 
 #getting their name
-name = gets.chomp
+name = gets.strip
 
 puts "Hi #{name}! You will be guessing a number between 1 and 10, and have three tries to get it right."
 
@@ -12,41 +12,43 @@ game_num = 8
 
 puts "What is your guess?"
 
-player_num = gets.chomp.to_i
+player_num = gets.strip.to_i
 
 counter = 1
 
 #loop for determining number of guesses and conditional for what to do next
 
-while counter < 4
+guesses = 3
+while counter <= guesses
 
 	if player_num == game_num
 
 		puts "Congratulations, you guessed it correctly!"
+		
 		break
 
 	elsif player_num > game_num
 
 		puts "Your number was a bit too high, try again"
-		counter = counter + 1
+		counter += 1
 
-		if counter == 4
+		if counter == 3
 			puts "Sorry you have lost the game. The number was #{game_num}."
 		else
-			puts "Number of guesses left: #{4 - counter}"
-			player_num = gets.chomp.to_i
+			puts "Number of guesses left: #{3 - counter}"
+			player_num = gets.strip.to_i
 		end
 
 	elsif player_num < game_num
 
 		puts "Your number was a bit too low, try again"
-		counter = counter + 1
+		counter += 1
 		
-		if counter == 4
+		if counter == 3
 			puts "Sorry you have lost the game. The number was #{game_num}."
 		else
-			puts "Number of guesses left: #{4 - counter}"
-			player_num = gets.chomp.to_i
+			puts "Number of guesses left: #{3 - counter}"
+			player_num = gets.strip.to_i
 		end
 
 	end
