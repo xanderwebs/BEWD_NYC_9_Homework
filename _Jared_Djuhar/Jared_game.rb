1f236e265e65c760148120422f18ad57917a6dcc
JARED DJUHAR <JAREDDJUHAR@GMAIL.COM>
2014-04-03 16:28:58 -0400

    Secret Number game

--- _Jared_Djuhar/Jared_game.rb
+++ _Jared_Djuhar/Jared_game.rb
@@ -1,42 +1,44 @@
+number = 7
+
 puts "Welcome to Jared's secret number game!"
 puts "You must guess a number between 1 and 10. Also, you will only have 3 tries"
 
 puts "Ok. Now guess a number"
 guess = gets.to_i
-if guess>7
+if guess> number
 	puts "Too high!"
-	elsif guess<7
+	elsif guess<number
 	puts "Too low!"
     else 
     puts "Congratulations you guessed the right number!"
 end
-if guess==7
+if guess==number
 	exit
 end
 
-puts "Ok. Now guess another number"
+puts "Don't give up. Guess another number"
 guess = gets.to_i
-if guess>7
+if guess>number
 	puts "Too high!"
-	elsif guess<7
+	elsif guess<number
 	puts "Too low!"
     else 
     puts "Congratulations you guessed the right number!"
 end
-if guess==7
+if guess==number
 	exit
 end
 
-puts "Ok. Now guess another number. Last try"
+puts "One last try"
 guess = gets.to_i
-if guess>7
+if guess>number
 	puts "Too high!"
-	elsif guess<7
+	elsif guess<number
 	puts "Too low!"
     else 
     puts "Congratulations you guessed the right number!"
 end
-if guess==7
+if guess==number
 	exit
 end
 
