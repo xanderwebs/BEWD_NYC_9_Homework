13361509dfe23f1fbdf6a62fc16cd32346285374
xanderwebs <xanderwebs@gmail.com>
2014-04-28 02:21:06 -0400

    Make class into viewer

--- _Alex_Cheng/midterm/lib/trello-api.rb
+++ _Alex_Cheng/midterm/lib/trello-api.rb
@@ -1,27 +1,67 @@
 require 'trello'
 
-class TrelloApi
-	attr_reader :board, :card, :list, :current_iterable
+class TrelloViewer
+	attr_reader :next, :level
 
 	def initialize auth
 		Trello.configure do |config|
 		  config.developer_public_key = auth['key']
 		  config.member_token = auth['token']
 		end
-		@board = Trello::Board.find(auth['board'])
-		@current_iterable = @board.lists
+		@history = []
+		@history << Trello::Board.find(auth['board'])
+		@next = self.peek.lists
+		@level = :board
 	end
 
-	def set_card id
-		@card = Trello::Card.find(id)
+	def add_card id
+		@history << Trello::Card.find(id)
+		@level = :card
+		@next = nil
 	end
 
-	def set_list id
-		@list = Trello::List.find(id)
-		@current_iterable = @list.cards
+	def add_list id
+		@history << Trello::List.find(id)
+		@level = :list
+		@next = self.peek.cards
 	end
 
-	def set_iterable obj
-		@current_iterable = obj
+	def has_next?
+		@history.empty? || !!@next
+	end
+
+	def get_choice
+		show_options @next
+		select_option @next.length-1
+	end
+
+	def peek
+		@history.last
+	end
+
+	private
+
+	def show_options opts
+		opts.each_with_index do |opt, i|
+			puts "#{i} #{opt.name}"
+		end
+	end
+
+	def select_option max_range
+		selection = get_selection
+		return selection if (0..max_range).include? selection
+
+		puts 'Hey wait, that\'s not a possible option!'
+		select_option max_range
+	end
+
+	def get_selection
+	  print 'Select an option: '
+	  begin
+	    Integer(gets.strip)
+	  rescue
+	    puts 'Hey, what are you trying to do here, that\'s not a number!'
+	    get_selection
+	  end
 	end
 end
--- _Alex_Cheng/midterm/runner.rb
+++ _Alex_Cheng/midterm/runner.rb
@@ -1,50 +1,22 @@
 require_relative 'lib/trello-api'
 require 'yaml'
-require 'pry'
 
 AUTH_KEYS = YAML.load_file('lib/auth_keys.yml')
-
 
 def welcome
 	puts 'Welcome to your Trello CLI browser. I\'ll be your host.'
 end
 
-def show_options opts
-	opts.each_with_index do |opt, i|
-		puts "#{i} #{opt.name}"
-	end
-end
-
-def get_selection
-  print 'Select an option: '
-  begin
-    Integer(gets.strip)
-  rescue
-    puts 'Hey, what are you trying to do here, that\'s not a number!'
-    get_selection
-  end
-end
-
-def select_option max_range
-	selection = get_selection
-	return selection if (0..max_range).include? selection
-
-	puts 'Hey wait, that\'s not a possible option!'
-	select_option max_range
-end
-
-def get_choices iterable
-	show_options iterable
-	select_option iterable.length-1
-end
-
 def main
-	t = TrelloApi.new AUTH_KEYS
-	choice = get_choices(t.current_iterable)
-	t.set_list t.current_iterable[choice].id
-	choice = get_choices(t.current_iterable)
-	t.set_card t.current_iterable[choice].id
-	puts "You selected #{t.card.name}"
+	t = TrelloViewer.new AUTH_KEYS
+  while true
+    puts "You chose the #{t.peek.name} #{t.level}"
+    break unless t.has_next?
+
+    choice = t.get_choice
+    next_choice = t.next[choice]
+    (t.level == :list) ? t.add_card(next_choice.id) : t.add_list(next_choice.id)
+  end
 end
 
 welcome
